a65b39f1ecf35a46be08e8234830945228dd531a
Leo J Penaloza <39505025+lpenaloza16@users.noreply.github.com>
2024-06-09 23:46:51 -0700

    x

--- snippets/src/db/index.ts
+++ snippets/src/db/index.ts
@@ -1,1 +1,14 @@
 import { PrismaClient } from "@prisma/client/extension";
+
+export const db = new PrismaClient();
+
+// Also state this is an index.ts in a folder named db
+//this db object is our portal/handle onto our database, we will use
+//to create save delete fetch all kings of things
+
+db.snippet.create({
+  data: {
+    title: "title",
+    code: "const abc = ()=>{}",
+  },
+});
